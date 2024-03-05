8bae495b2c870ae1fbdc41614ad9bfadde672264
shahadat <shahadathossan937@gmail.com>
2024-03-05 13:33:21 +0600

    add enum & helper folder

--- Hrm.Api/GlobalUsings.cs
+++ Hrm.Api/GlobalUsings.cs
@@ -3,8 +3,8 @@ global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using Hrm.Application.Responses;
-global using Hrm.Application.DTOs;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
+

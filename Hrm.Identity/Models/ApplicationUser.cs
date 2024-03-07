766de5862630001085b2a53bc80336d675308746
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-03-07 16:09:16 +0600

    ApplicationUser.cs file nullable updated

--- Hrm.Identity/Models/ApplicationUser.cs
+++ Hrm.Identity/Models/ApplicationUser.cs
@@ -10,13 +10,13 @@ namespace Hrm.Identity.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string RoleName { get; set; }       
-        public string PNo { get; set; }
-        public string BranchId { get; set; }
+        public string? RoleName { get; set; }       
+        public string? PNo { get; set; }
+        public string? BranchId { get; set; }
         public bool IsActive { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
-        public string InActiveBy { get; set; }
+        public string? InActiveBy { get; set; }
         public DateTime? InActiveDate { get; set; }
     }
 }

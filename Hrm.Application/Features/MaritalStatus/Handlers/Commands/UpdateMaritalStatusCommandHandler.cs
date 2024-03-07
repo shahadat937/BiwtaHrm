2ad610fd61095868fc6a97dfe40a9ced1fb43fb8
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-03-07 17:47:16 +0600

    KAN-25 => KAN-169 (Marital Status Edit API Design) Completed

---
+++ Hrm.Application/Features/MaritalStatus/Handlers/Commands/UpdateMaritalStatusCommandHandler.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using FluentValidation.Results;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.DTOs.MaritalStatus.Validators;
+using Hrm.Application.Exceptions;
+using Hrm.Application.Features.MaritalStatus.Requests.Commands;
+using Hrm.Application.Responses;
+using Hrm.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.MaritalStatus.Handlers.Commands
+{
+    public class UpdateMaritalStatusCommandHandler : IRequestHandler<UpdateMaritalStatusCommand, Unit>
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UpdateMaritalStatusCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateMaritalStatusCommand request, CancellationToken cancellationToken)
+        {
+            var respose = new BaseCommandResponse();
+            var validator = new UpdateMaritalStatusDtoValidator();
+            var validationResult = await validator.ValidateAsync(request.MaritalStatusDto);
+
+            if (validationResult.IsValid == false)
+            {
+                respose.Success = false;
+                respose.Message = "Creation Failed";
+                respose.Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
+            }
+
+            var MaritalStatus = await _unitOfWork.Repository<Hrm.Domain.MaritalStatus>().Get(request.MaritalStatusDto.MaritalStatusId);
+
+            if (MaritalStatus is null)
+            {
+                throw new NotFoundException(nameof(MaritalStatus), request.MaritalStatusDto.MaritalStatusId);
+            }
+
+            _mapper.Map(request.MaritalStatusDto, MaritalStatus);
+
+            await _unitOfWork.Repository<Hrm.Domain.MaritalStatus>().Update(MaritalStatus);
+            await _unitOfWork.Save();
+
+            return Unit.Value;
+        }
+    }
+}

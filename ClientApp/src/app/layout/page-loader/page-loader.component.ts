6817a1b1ba6fd4cef653c552c88703960a29cdc6
shahadat <shahadathossan937@gmail.com>
2024-03-10 18:46:43 +0600

    layout folder structure add on client project

---
+++ ClientApp/src/app/layout/page-loader/page-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-loader.component.html',
+  styleUrl: './page-loader.component.css'
+})
+export class PageLoaderComponent {
+
+}

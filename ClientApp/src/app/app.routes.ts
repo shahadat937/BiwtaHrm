b2c1bac99da04e5a74a2dc39b3cbaf7e74963824
shahadat <shahadathossan937@gmail.com>
2024-03-12 14:49:22 +0600

    "modify client project"

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/app-layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+    },
+    
+   
+];

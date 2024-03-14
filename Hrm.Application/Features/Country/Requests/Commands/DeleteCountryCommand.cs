363285f843a15e55280e643e1e3e7f1c1abc6568
Ariful Islam <mr.arif.itil@gmail.com>
2024-03-14 13:07:38 +0600

    Jira KAN-269 Country API Design Delete done

---
+++ Hrm.Application/Features/Country/Requests/Commands/DeleteCountryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hrm.Application.Features.Stores.Requests.Commands
+{
+    public class DeleteCountryCommand : IRequest
+    {
+        public int CountryId { get; set; }
+    }
+}

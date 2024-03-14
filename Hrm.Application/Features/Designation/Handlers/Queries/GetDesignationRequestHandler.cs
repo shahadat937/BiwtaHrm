a276108ab99a566794274576859100f2d6151479
Ariful Islam <mr.arif.itil@gmail.com>
2024-03-14 11:36:30 +0600

    Jira KAN-62 AND KAN-62 (Designation API full Crud) done

---
+++ Hrm.Application/Features/Designation/Handlers/Queries/GetDesignationRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.DTOs.Designation;
+using Hrm.Application.DTOs.Designation;
+using Hrm.Application.Exceptions;
+using Hrm.Application.Features.Designation.Requests.Queries;
+using Hrm.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Designation.Handlers.Queries
+{
+    public class GetDesignationRequestHandler : IRequestHandler<GetDesignationRequest, object>
+    {
+
+        private readonly IHrmRepository<Hrm.Domain.Designation> _DesignationRepository;
+        private readonly IMapper _mapper;
+        public GetDesignationRequestHandler(IHrmRepository<Hrm.Domain.Designation> DesignationRepository, IMapper mapper)
+        {
+            _DesignationRepository = DesignationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<object> Handle(GetDesignationRequest request, CancellationToken cancellationToken)
+        {
+            IQueryable<Hrm.Domain.Designation> Designation = _DesignationRepository.Where(x => true);
+
+            var DesignationDtos = _mapper.Map<List<DesignationDto>>(Designation);
+
+            return DesignationDtos;
+        }
+    }
+}

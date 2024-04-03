7b9c21f8b02195d07f04e292f1903d3ca55906e9
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-03 12:08:39 +0600

    ChildStatus EmployeeType Gender MaritalStatus Religion service API endpoint created

--- ClientApp/src/app/views/basic-setup/service/child-status.service.ts
+++ ClientApp/src/app/views/basic-setup/service/child-status.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { ChildStatus } from '../model/child-status';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChildStatusService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+  childStatus: ChildStatus;
+  constructor(private http: HttpClient) {
+    this.childStatus = new ChildStatus();
+   }
+  find(id: number) {
+    return this.http.get<ChildStatus>(this.baseUrl + '/childStatus/get-ChildStatusById/' + id);
+  }
+  getAll():Observable<ChildStatus[]> {
+    return this.http.get<ChildStatus[]>(this.baseUrl + '/childStatus/get-ChildStatus');
+  }
+  update(id: number,model: any) {
+    return this.http.put(this.baseUrl + '/childStatus/update-ChildStatus/'+id, model);
+  }
+  submit(model: any) {
+    return this.http.post(this.baseUrl + '/childStatus/save-ChildStatus', model);
+  }
+  delete(id:number){
+    return this.http.delete(this.baseUrl + '/childStatus/delete-ChildStatus/'+id);
+  }
 }
--- ClientApp/src/app/views/basic-setup/service/gender.service.ts
+++ ClientApp/src/app/views/basic-setup/service/gender.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Gender } from '../model/gender';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenderService {
-
-  constructor() { }
+  baseUrl = environment.apiUrl;
+  gender: Gender;
+  constructor(private http: HttpClient) {
+    this.gender = new Gender();
+   }
+  find(id: number) {
+    return this.http.get<Gender>(this.baseUrl + '/gender/get-genderById/' + id);
+  }
+  getAll():Observable<Gender[]> {
+    return this.http.get<Gender[]>(this.baseUrl + '/gender/get-gender');
+  }
+  update(id: number,model: any) {
+    return this.http.put(this.baseUrl + '/gender/update-gender/'+id, model);
+  }
+  submit(model: any) {
+    return this.http.post(this.baseUrl + '/gender/save-gender', model);
+  }
+  delete(id:number){
+    return this.http.delete(this.baseUrl + '/gender/delete-gender/'+id);
+  }
 }

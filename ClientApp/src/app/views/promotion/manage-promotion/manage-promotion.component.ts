cb59ba8276226f304ab6451d2bc3636861820d2a
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-24 13:24:31 +0600

    manage-promotion promotion component creation done on promotion module

---
+++ ClientApp/src/app/views/promotion/manage-promotion/manage-promotion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-promotion',
+  templateUrl: './manage-promotion.component.html',
+  styleUrl: './manage-promotion.component.scss'
+})
+export class ManagePromotionComponent {
+
+}
--- ClientApp/src/app/views/promotion/promotion.module.ts
+++ ClientApp/src/app/views/promotion/promotion.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PromotionRoutingModule } from './promotion-routing.module';
+import { PromotionComponent } from './promotion/promotion.component';
+import { ManagePromotionComponent } from './manage-promotion/manage-promotion.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PromotionComponent,
+    ManagePromotionComponent
+  ],
   imports: [
     CommonModule,
     PromotionRoutingModule

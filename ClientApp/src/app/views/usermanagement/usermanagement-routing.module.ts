5a0f5ecd2d456f4c75068701e4206bd21085a312
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-24 12:07:07 +0600

    usermanagement module creation done

---
+++ ClientApp/src/app/views/usermanagement/usermanagement-routing.module.ts
@@ -0,0 +1,16 @@
+import { UserComponent } from './user/user.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsermanagementRoutingModule { }

9c73327dcb8ce288dc4353d50b4392af8b8019cd
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-24 12:41:59 +0600

    add-employee  manage-employee component creation done on employee.module.ts

--- ClientApp/src/app/views/employee/employee.module.ts
+++ ClientApp/src/app/views/employee/employee.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ManageEmployeeComponent } from './manage-employee/manage-employee.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddEmployeeComponent,
+    ManageEmployeeComponent
+  ],
   imports: [
     CommonModule,
     EmployeeRoutingModule

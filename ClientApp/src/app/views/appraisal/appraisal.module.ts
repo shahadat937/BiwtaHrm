32995fe82d1af73a2fe2b730fa4b8fa7aa28d74d
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-24 12:38:04 +0600

    appraisal  attendance  employee  leave  promotion  transfer module ctreation done

---
+++ ClientApp/src/app/views/appraisal/appraisal.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppraisalRoutingModule } from './appraisal-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppraisalRoutingModule
+  ]
+})
+export class AppraisalModule { }

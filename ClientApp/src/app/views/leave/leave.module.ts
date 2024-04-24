91dba0be8264f3a77cbc0d5067f6efe76324ed6d
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-04-24 13:19:24 +0600

    addleave  manageleave done

--- ClientApp/src/app/views/leave/leave.module.ts
+++ ClientApp/src/app/views/leave/leave.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LeaveRoutingModule } from './leave-routing.module';
+import { AddleaveComponent } from './addleave/addleave.component';
+import { ManageleaveComponent } from './manageleave/manageleave.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddleaveComponent,
+    ManageleaveComponent
+  ],
   imports: [
     CommonModule,
     LeaveRoutingModule

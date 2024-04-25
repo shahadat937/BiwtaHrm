5add050f5fda2f3cf443049b470ccf82b8ce0502
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2024-04-25 23:24:28 +0600

    Employee View Information List Created

--- ClientApp/src/app/views/employee/employee-routing.module.ts
+++ ClientApp/src/app/views/employee/employee-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ManageEmployeeComponent } from './manage-employee/manage-employee.component';
 import { ViewUsersComponent } from './add-employee/view-users/view-users.component';
 import { ViewInformationListComponent } from './add-employee/view-information-list/view-information-list.component';
+import { BasicInformationComponent } from './add-employee/employee-informations/basic-information/basic-information.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'view-imformation-list',
     component: ViewInformationListComponent,
+  },
+  {
+    path: 'basic-information',
+    component: BasicInformationComponent,
   },
 ];
 

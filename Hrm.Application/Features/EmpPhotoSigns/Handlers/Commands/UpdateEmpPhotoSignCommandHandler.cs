6bb70578bbe546a9e17cefbbdae261528e3a0e0d
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-06-22 16:54:18 +0600

    Working with EmpPhotoSing in Employee Module

---
+++ Hrm.Application/Features/EmpPhotoSigns/Handlers/Commands/UpdateEmpPhotoSignCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.Features.EmpPersonalInfos.Requests.Commands;
+using Hrm.Application.Features.EmpPhotoSigns.Requests.Commands;
+using Hrm.Application.Responses;
+using Hrm.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.EmpPhotoSigns.Handlers.Commands
+{
+    public class UpdateEmpPhotoSignCommandHandler : IRequestHandler<UpdateEmpPhotoSignCommand, BaseCommandResponse>
+    {
+        private readonly IHrmRepository<EmpPhotoSign> _EmpPersonalInfoRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UpdateEmpPhotoSignCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, IHrmRepository<EmpPhotoSign> EmpPersonalInfoRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _EmpPersonalInfoRepository = EmpPersonalInfoRepository;
+        }
+
+        public async Task<BaseCommandResponse> Handle(UpdateEmpPhotoSignCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseCommandResponse();
+            var EmpPhotoSign = await _unitOfWork.Repository<EmpPhotoSign>().Get(request.EmpPhotoSignDto.Id);
+
+            string uniqueImageName = request.EmpPhotoSignDto.PNo + "_Photo";
+            var a = Directory.GetCurrentDirectory();
+            var photoPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\assets\\Images\\EmpPhoto", uniqueImageName);
+
+
+
+            _mapper.Map(request.EmpPhotoSignDto, EmpPhotoSign);
+
+            await _unitOfWork.Repository<EmpPhotoSign>().Update(EmpPhotoSign);
+            await _unitOfWork.Save();
+
+            response.Success = true;
+            response.Message = "Update Successfull";
+
+            return response;
+        }
+
+    }
+}
---
+++ Hrm.Domain/EmpPhotoSign.cs
@@ -0,0 +1,23 @@
+﻿using Hrm.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Domain
+{
+    public class EmpPhotoSign : BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public int EmpId { get; set; }
+        public string? PhotoUrl { get; set; }
+        public string? SignatureUrl { get; set; }
+        public string? UniqueIdentity { get; set; }
+        public string? Remark { get; set; }
+        public int? MenuPosition { get; set; }
+        public bool? IsActive { get; set; }
+
+        //public virtual EmpBasicInfo? EmpBasicInfo { get; set; }
+    }
+}

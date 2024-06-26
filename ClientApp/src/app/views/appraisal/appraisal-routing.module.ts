0e12f3fd1f1880d058d6509dac34e8973ebcab31
OptimisticNurul-InfinityTechLtd <nurulislam@infinitytechltd.com>
2024-06-26 17:52:21 +0600

    ACR Staff Form (part-1 & part-2 template design) on appraisal module

--- ClientApp/src/app/views/appraisal/appraisal-routing.module.ts
+++ ClientApp/src/app/views/appraisal/appraisal-routing.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StaffFormComponent } from './staff-form/staff-form.component';
 import { OfficerFormComponent } from './officer-form/officer-form.component';
+import { StaffForm2Component } from './staff-form/staff-form-2/staff-form-2.component';
+import { StaffForm3Component } from './staff-form/staff-form-3/staff-form-3.component';
 
 const routes: Routes = [
   {
@@ -17,6 +19,16 @@ const routes: Routes = [
   {
     path:'officerForm',
     component:OfficerFormComponent
+  }
+  ,
+  {
+    path: "staffForm2",
+    component:StaffForm2Component
+  }
+  ,
+  {
+    path: "staffForm3",
+    component:StaffForm3Component
   }
 ];
 

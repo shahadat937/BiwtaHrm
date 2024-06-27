d4274d6fcfa046faf1ab7f78f7346aad7e547a9f
Joy <joymahanta9@infinitytechltd.com>
2024-06-27 17:53:08 +0600

    Added Workday dto, domain and create, get, getbyid features

---
+++ Hrm.Application/DTOs/Workday/Validations/CreateWorkdayDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.DTOs.Workday.Validations
+{
+    public class CreateWorkdayDtoValidator : AbstractValidator<CreateWorkdayDto>
+    {
+        public CreateWorkdayDtoValidator() {
+            Include(new IWorkdayDtoValidator());
+        }
+    }
+}

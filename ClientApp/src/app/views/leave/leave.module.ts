d1b3eb04eed90bc49acf10efd35c70eb9995dfb6
OptimisticNurul-InfinityTechLtd <nurulislam@infinitytechltd.com>
2024-06-30 18:09:33 +0600

    Add leave & Manage Leave form template Design

--- ClientApp/src/app/views/leave/leave.module.ts
+++ ClientApp/src/app/views/leave/leave.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  CollapseModule,
+  DropdownModule,
+  FormModule,
+  GridModule,
+  ListGroupModule,
+  ProgressModule,
+  SharedModule,
+  TableModule,
+  UtilitiesModule,
+  PaginationModule
+} from '@coreui/angular';
 
 import { LeaveRoutingModule } from './leave-routing.module';
 import { AddleaveComponent } from './addleave/addleave.component';
 import { ManageleaveComponent } from './manageleave/manageleave.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +27,21 @@ import { ManageleaveComponent } from './manageleave/manageleave.component';
   ],
   imports: [
     CommonModule,
-    LeaveRoutingModule
+    LeaveRoutingModule,
+    CardModule,
+    FormModule,
+    GridModule,
+    ButtonModule,
+    ButtonGroupModule,
+    DropdownModule,
+    SharedModule,
+    ListGroupModule,
+    ProgressModule,
+    CollapseModule,
+    TableModule,
+    UtilitiesModule,
+    PaginationModule,
+    
   ]
 })
 export class LeaveModule { }

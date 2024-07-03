853fd992fcb9b3afbe87de50c71a41ea007b861e
yasin-Infinity <yasinali854@infinitytechltd.com>
2024-07-03 18:17:27 +0600

    appraisal module part4-7 on problem in part-5 data do not show console

---
+++ ClientApp/src/app/views/appraisal/officer-form/model/officer-form-part4.module.ts
@@ -0,0 +1,10 @@
+export class OfficerFormPart4Module { 
+  personalCharacteristics: string='' ;
+  professionalSkill: string='' ;
+  loyalityAndRelaiability: string='' ;
+  otherMatters :string='';
+  annualConfidentialReportWritingAndCountersigningActivates :string='' ;
+  adviceToOfficers :string='' ;
+  proficencyAndInterestBengaliLanguage :string='' ;
+  signatureOfReportingOfficer :File | null = null;
+}
---
+++ ClientApp/src/app/views/appraisal/officer-form/model/officer-form-part5.module.ts
@@ -0,0 +1,13 @@
+
+export class OfficerFormPart5Module { 
+  setpeciality:string='';
+  suitabilityForFurtherEmployment :string='';
+  recommendMoreServiceTraning:string='';
+  notEligableForFurtherPromotion:string='';
+  fasterPromotionIsRecomended :File | null = null;
+  eligibleForPromotion :File | null = null;
+  recentlyPromotedNotDueForFurtherPromotion :File | null = null;
+  notyetEligableForPromotionButWillBeDueCourse :File | null = null;
+  notEligableForFurtherPromotionMaximumLimitReached :File | null = null;
+  signature :File | null = null;
+ }

25e79883d8da8f949d2311e774a0db44d528e76e
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-07-08 18:11:11 +0600

    Working with Employee Id card

---
+++ ClientApp/src/app/views/employee/manage-employee/emp-id-card-generate/emp-id-card-generate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-id-card-generate',
+  templateUrl: './emp-id-card-generate.component.html',
+  styleUrl: './emp-id-card-generate.component.scss'
+})
+export class EmpIdCardGenerateComponent {
+
+  
+
+}

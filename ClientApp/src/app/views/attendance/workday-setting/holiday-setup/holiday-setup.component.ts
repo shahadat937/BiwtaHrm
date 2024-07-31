9c3bc47127fc2a79e8ecaeb88692b618339af8e3
Joy <joymahanta9@infinitytechltd.com>
2024-07-31 17:05:23 +0600

    Added HolidaySetup submit UI

--- ClientApp/src/app/views/attendance/workday-setting/holiday-setup/holiday-setup.component.ts
+++ ClientApp/src/app/views/attendance/workday-setting/holiday-setup/holiday-setup.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { ConfirmService } from 'src/app/core/service/confirm.service';
 
 @Component({
   selector: 'app-holiday-setup',
-  standalone: true,
-  imports: [],
   templateUrl: './holiday-setup.component.html',
   styleUrl: './holiday-setup.component.scss'
 })
-export class HolidaySetupComponent {
+export class HolidaySetupComponent implements OnInit, OnDestroy {
+  isVisible: boolean
+  holidayTypeOption : any[] = [];
+  subscription: Subscription = new Subscription();
+
+  constructor(
+    private toastr: ToastrService,
+    private confirmService: ConfirmService
+  ) {
+    this.isVisible=true;
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+    } 
+  }
+
+  getHolidayType() {
+    return ""; 
+  }
 
 }

3ebada7d12cd4a15819589f89c8d907d08472cc1
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-08-06 13:28:03 +0600

    Added LeaveRequest Dto, Domain

---
+++ Hrm.Domain/LeaveRequest.cs
@@ -0,0 +1,29 @@
+﻿using Hrm.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Domain
+{
+    public class LeaveRequest: BaseDomainEntity
+    {
+        public int LeaveRequestId { get; set; }
+        public int EmpId { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateOnly FromDate { get; set; }
+        public DateOnly ToDate { get; set; }
+        public string? LeavePurpose { get; set; }
+        public bool IsForeignLeave { get; set; }
+        public int? CountryId { get; set; }
+        public string? ForeignLeavePurpose { get; set; }
+        public string? AccompanyBy { get; set; }
+        public string? AssociatedFile { get; set; }
+        public int? Status { get; set; }
+        public bool IsActive { get; set; }
+        public string? Remark { get; set; }
+        public int? MenuPosition { get; set; }
+        
+    }
+}

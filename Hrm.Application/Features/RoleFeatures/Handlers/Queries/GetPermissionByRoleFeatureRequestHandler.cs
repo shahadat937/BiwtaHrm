3c42689b973177f91bbfacc5f6264ab1d4b1d5e3
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-08-20 17:30:42 +0600

    Login page look modified and remember me functionality added

--- Hrm.Application/Features/RoleFeatures/Handlers/Queries/GetPermissionByRoleFeatureRequestHandler.cs
+++ Hrm.Application/Features/RoleFeatures/Handlers/Queries/GetPermissionByRoleFeatureRequestHandler.cs
@@ -15,14 +15,21 @@ namespace Hrm.Application.Features.RoleFeatures.Handlers.Queries
     public class GetPermissionByRoleFeatureRequestHandler : IRequestHandler<GetPermissionByRoleFeatureRequest, FeaturePermissionDto>
     {
         private readonly IHrmRepository<RoleFeature> _RoleFeaturesRepository;
-        public GetPermissionByRoleFeatureRequestHandler(IHrmRepository<RoleFeature> RoleFeaturesRepository)
+        private readonly IHrmRepository<Feature> _FeaturesRepository;
+        private readonly IHrmRepository<AspNetRoles> _AspNetRolesRepository;
+        public GetPermissionByRoleFeatureRequestHandler(IHrmRepository<RoleFeature> RoleFeaturesRepository, IHrmRepository<Feature> FeaturesRepository, IHrmRepository<AspNetRoles> AspNetRolesRepository)
         {
             _RoleFeaturesRepository = RoleFeaturesRepository;
+            _AspNetRolesRepository = AspNetRolesRepository;
+            _FeaturesRepository = FeaturesRepository;
         }
 
         public async Task<FeaturePermissionDto> Handle(GetPermissionByRoleFeatureRequest request, CancellationToken cancellationToken)
         {
-            var roleFeature = await _RoleFeaturesRepository.FindOneAsync(x => x.RoleName == request.RoleName && x.FeaturePath == request.FeaturePath);
+            var roleId = await _AspNetRolesRepository.FindOneAsync(x => x.Name == request.RoleName);
+            var featureId = await _FeaturesRepository.FindOneAsync(x => x.Path == request.FeaturePath);
+
+            var roleFeature = await _RoleFeaturesRepository.FindOneAsync(x => x.RoleId == roleId.Id && x.FeatureKey == featureId.FeatureId);
 
             var result = new FeaturePermissionDto
             {

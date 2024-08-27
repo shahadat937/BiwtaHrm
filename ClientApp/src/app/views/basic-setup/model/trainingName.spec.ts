f1bc5263b6a9a62af361204fbebbde7e5f3ffed1
Md Shahadat Hossan <shahadathossan937@gmail.com>
2024-08-27 15:31:38 +0600

    Remove unnecessary component

--- ClientApp/src/app/views/basic-setup/model/trainingName.spec.ts
+++ ClientApp/src/app/views/basic-setup/model/trainingName.spec.ts
@@ -1,7 +1,3 @@
-import { TrainingName } from './training-name';
 
-describe('TrainingName', () => {
-  it('should create an instance', () => {
-    expect(new TrainingName()).toBeTruthy();
-  });
-});
+
+

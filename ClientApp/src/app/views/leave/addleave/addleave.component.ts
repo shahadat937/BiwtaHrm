2deb5d3f71ac873f29bf258483f9e3580d5c0a0b
Joy <joymahanta9@infinitytechltd.com>
2024-09-07 14:01:50 +0600

    Hide the foreign leave entry in add leave until foreign leave type is selected

--- ClientApp/src/app/views/leave/addleave/addleave.component.ts
+++ ClientApp/src/app/views/leave/addleave/addleave.component.ts
@@ -130,9 +130,9 @@ export class AddleaveComponent  implements OnInit, OnDestroy{
   }
 
   getLeaveAmount() {
+    this.IsForeignLeave();
     console.log(this.addLeaveService.addLeaveModel);
     if(this.addLeaveService.addLeaveModel.empId==null||this.addLeaveService.addLeaveModel.leaveTypeId==null||this.addLeaveService.addLeaveModel.fromDate==null||this.addLeaveService.addLeaveModel.toDate==null) {
-      console.log("Hello World");
       this.totalDue = null;
       return;
     }
@@ -251,4 +251,20 @@ export class AddleaveComponent  implements OnInit, OnDestroy{
   handleFile(event:any) {
     this.addLeaveService.addLeaveModel.associatedFiles = event.target.files[0];
   }
+
+  IsForeignLeave() {
+
+    let leaveName = this.LeaveTypeOption.find(x=>x.id == this.addLeaveService.addLeaveModel.leaveTypeId);
+    if(leaveName == undefined) {
+      this.addLeaveService.addLeaveModel.isForeignLeave = false;
+      return;
+    }
+
+    if(leaveName.name.toLocaleLowerCase().includes("foreign")) {
+      this.addLeaveService.addLeaveModel.isForeignLeave = true;
+    } else {
+      this.addLeaveService.addLeaveModel.isForeignLeave = false;
+    }
+  }
+
 }

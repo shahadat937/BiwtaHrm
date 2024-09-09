1702efc4e6a59e0f72cad10b5cb4ca8fec87224e
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-09-09 17:52:42 +0600

    Getting Attendance Infor Widget completed

--- Hrm.Application/DTOs/Dashboard/Widgets/WidgetsDto.cs
+++ Hrm.Application/DTOs/Dashboard/Widgets/WidgetsDto.cs
@@ -9,7 +9,7 @@ namespace Hrm.Application.DTOs.Dashboard.Widgets
     public class WidgetsDto
     {
         public string? WidgetName { get; set; }
-        public string? Lebel { get; set; }
+        public string? Label { get; set; }
         public List<string>? Labels { get; set; }
         public List<int>? Data { get; set; }
         public int? TotalPercentage { get; set; }

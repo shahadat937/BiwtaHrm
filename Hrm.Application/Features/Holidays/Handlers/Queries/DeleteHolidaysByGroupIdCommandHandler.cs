7d0618bcaf09ec2566d2b40c48e58d757a5f9adb
Joy <joymahanta9@infinitytechltd.com>
2024-09-11 18:49:15 +0600

    Added Delete and update by groupId in holidays

---
+++ Hrm.Application/Features/Holidays/Handlers/Queries/DeleteHolidaysByGroupIdCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.Features.appraisalFormType.Requests.Commands;
+using Hrm.Application.Features.Holidays.Requests.Commands;
+using Hrm.Application.Responses;
+using Hrm.Application.Exceptions;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Holidays.Handlers.Queries
+{
+    public class DeleteHolidaysByGroupIdCommandHandler: IRequestHandler<DeleteHolidaysByGroupIdCommand, BaseCommandResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public DeleteHolidaysByGroupIdCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseCommandResponse> Handle(DeleteHolidaysByGroupIdCommand request, CancellationToken cancellationToken)
+        {
+            List<Hrm.Domain.Holidays> holidays = await _unitOfWork.Repository<Hrm.Domain.Holidays>().Where(x => x.GroupId == request.GroupId).ToListAsync();
+
+
+            if(holidays.Count<=0)
+            {
+                throw new NotFoundException(nameof(holidays), request.GroupId);
+            }
+
+            foreach(var holiday in holidays)
+            {
+                await _unitOfWork.Repository<Hrm.Domain.Holidays>().Delete(holiday);
+            }
+
+            await _unitOfWork.Save();
+
+            var response = new BaseCommandResponse();
+            response.Success = true;
+            response.Message = "Delete Successful";
+            response.Id = request.GroupId;
+
+            return response;
+        }
+    }
+}
---
+++ Hrm.Application/Features/Holidays/Requests/Commands/UpdateHolidaysByGroupIdCommand.cs
@@ -0,0 +1,19 @@
+﻿using Hrm.Application.DTOs.Holidays;
+using Hrm.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Holidays.Requests.Commands
+{
+    public class UpdateHolidaysByGroupIdCommand: IRequest<BaseCommandResponse>
+    {
+        public int GroupId { get; set; }
+        public DateOnly From { get; set; }
+        public DateOnly To { get; set; }
+        public CreateHolidayDto HolidayDto { get; set; }
+    }
+}

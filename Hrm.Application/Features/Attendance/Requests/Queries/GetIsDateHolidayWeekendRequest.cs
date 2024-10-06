14a0acea3c6f934e8428b137d181abb42443c326
Joy <joymahanta9@infinitytechltd.com>
2024-10-06 17:16:18 +0600

    Added Route to for getting the holidays and weekend data for indicating in the attendance report

---
+++ Hrm.Application/Features/Attendance/Requests/Queries/GetIsDateHolidayWeekendRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Attendance.Requests.Queries
+{
+    public class GetIsDateHolidayWeekendRequest: IRequest<object>
+    {
+        public DateOnly From {  get; set; }
+        public DateOnly To { get; set; }
+    }
+}

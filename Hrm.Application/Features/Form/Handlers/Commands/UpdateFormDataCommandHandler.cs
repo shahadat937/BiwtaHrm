9bbd55f9fdab7d7714def21e0d2cd871903d92cf
Joy <joymahanta9@infinitytechltd.com>
2024-10-21 12:50:27 +0600

    Modified the appraisal form update handler to support for the form group

--- Hrm.Application/Features/Form/Handlers/Commands/UpdateFormDataCommandHandler.cs
+++ Hrm.Application/Features/Form/Handlers/Commands/UpdateFormDataCommandHandler.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Hrm.Application.DTOs.FieldRecord;
 using Hrm.Application.Enum;
+using Hrm.Application.DTOs.Form;
 
 namespace Hrm.Application.Features.Form.Handlers.Commands
 {
@@ -47,10 +48,12 @@ namespace Hrm.Application.Features.Form.Handlers.Commands
             }
 
             
+
+            // Validate if the form can be modified according to the current form status and the user
             if(request.UpdateRole == (int) AppraisalRole.User)
             {
 
-            } else if(request.UpdateRole == (int) AppraisalRole.ReportingOfficer)
+            } else if(request.UpdateRole == (int) AppraisalRole.ReportingOfficer) // 
             {
                 if(formRecord.CounterSignatoryApproval)
                 {
@@ -82,20 +85,7 @@ namespace Hrm.Application.Features.Form.Handlers.Commands
             {
                 foreach(var field in section.Fields)
                 {
-                    if(field.FieldValue!=""&&!await _formHelper.CheckValidField(request.formData.FormId,field.FieldId))
-                    {
-                        throw new BadRequestException("Invalid Field: "+field.FieldName);
-                    }
-
-                    if(field.FieldValue!=""&&!await _formHelper.CheckDataType(field.FieldValue,field.FieldId))
-                    {
-                        throw new BadRequestException("Field Value (" + field.FieldName + ") is invalid");
-                    }
-
-                    if(field.FieldValue!=""&&!await _formHelper.IsValidSelectable(field.FieldValue,field.FieldId))
-                    {
-                        throw new BadRequestException("Field Option (" + field.FieldName + ") is invalid");
-                    }
+                    await CheckValidField(field, formRecord.RecordId);
 
                     var fieldDto = new FieldRecordDto();
                     fieldDto.FieldId = field.FieldId;
@@ -112,6 +102,34 @@ namespace Hrm.Application.Features.Form.Handlers.Commands
                         throw new NotFoundException(nameof(fieldRecord), fieldDto.FieldRecordId);
                     }
 
+                    if(field.HTMLTagName == "daterange")
+                    {
+                        foreach(var childField in field.ChildFields)
+                        {
+                            await CheckValidField(childField,formRecord.RecordId);
+
+                            var childFieldRecord = await _unitOfWork.Repository<Hrm.Domain.FieldRecord>().Get(childField.FieldRecordId);
+
+                            if(childFieldRecord==null)
+                            {
+                                throw new NotFoundException(nameof(fieldRecord), fieldDto.FormRecordId);
+                            }
+
+                            var childFieldDto = new FieldRecordDto();
+                            childFieldDto.FieldId = childField.FieldId;
+                            childFieldDto.FieldName = childField.FieldName;
+                            childFieldDto.FieldRecordId = childField.FieldRecordId;
+                            childFieldDto.FieldValue = childField.FieldValue;
+                            childFieldDto.FormRecordId = formRecord.RecordId;
+                            childFieldDto.Remark = childField.Remark;
+
+                            _mapper.Map(childFieldDto, childFieldRecord);
+                            await _unitOfWork.Repository<Hrm.Domain.FieldRecord>().Update(childFieldRecord);
+                        }
+                    }
+
+
+
                     _mapper.Map(fieldDto, fieldRecord);
                     await _unitOfWork.Repository<Hrm.Domain.FieldRecord>().Update(fieldRecord);
                 }
@@ -125,6 +143,27 @@ namespace Hrm.Application.Features.Form.Handlers.Commands
 
 
             return response;
+        }
+
+        public async Task CheckValidField(FormFieldValDto field, int recordId)
+        {
+            if (field.FieldValue == "")
+                return;
+
+            //if (field.FieldValue != "" && !await _formHelper.CheckValidField(request.formData.FormId, field.FieldId))
+            //{
+              //  throw new BadRequestException("Invalid Field: " + field.FieldName);
+            //}
+
+            if (field.FieldValue != "" && !await _formHelper.CheckDataType(field.FieldValue, field.FieldId))
+            {
+                throw new BadRequestException("Field Value (" + field.FieldName + ") is invalid");
+            }
+
+            if (field.FieldValue != "" && !await _formHelper.IsValidSelectable(field.FieldValue, field.FieldId))
+            {
+                throw new BadRequestException("Field Option (" + field.FieldName + ") is invalid");
+            }
         }
     }
 }

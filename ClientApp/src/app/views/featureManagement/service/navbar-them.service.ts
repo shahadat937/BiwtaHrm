543401f8d8b3e6639dba1149acbe1db06c19ded6
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-10-23 16:25:11 +0600

    NavbarThem CRUD Completed in UI

--- ClientApp/src/app/views/featureManagement/service/navbar-them.service.ts
+++ ClientApp/src/app/views/featureManagement/service/navbar-them.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SelectedModel } from 'src/app/core/models/selectedModel';
+import { environment } from 'src/environments/environment';
+import { NavbarThem } from '../model/navbar-them';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavbarThemService {
+  cachedData: any[] = [];
+  baseUrl = environment.apiUrl;
+  navbarThem: NavbarThem;
+  constructor(private http: HttpClient) {
+    this.navbarThem = new NavbarThem();
+   }
 
-  constructor() { }
+   
+  find(id: number) {
+    return this.http.get<NavbarThem>(this.baseUrl + '/navbarThem/get-NavbarThembyid/' + id);
+  }
+
+  getAll() {
+      return this.http.get<NavbarThem[]>(this.baseUrl + '/navbarThem/get-NavbarThem');
+  }
+
+  
+  submit(model: any) {
+    return this.http.post(this.baseUrl + '/navbarThem/save-NavbarThem', model);
+  }
+
+  update(id: number,model: any){
+    return this.http.put(this.baseUrl + '/navbarThem/update-NavbarThem/'+id, model);
+  }
+  
+  delete(id: number) {
+    return this.http.delete(this.baseUrl + '/navbarThem/delete-NavbarThem/' + id);
+  }
+
+  getSelectedNavbarThem() {
+    return this.http.get<SelectedModel[]>(this.baseUrl + '/navbarThem/get-selectedNavbarThem');
+  }
+
 }

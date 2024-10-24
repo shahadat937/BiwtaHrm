32932744742e5e6df60b7c61a575f90f7c4f41ae
Joy <joymahanta9@infinitytechltd.com>
2024-10-24 17:51:40 +0600

    Fixed: In Leave Type Setting checkbox not working properly

--- ClientApp/src/app/views/basic-setup/model/leave-type.ts
+++ ClientApp/src/app/views/basic-setup/model/leave-type.ts
@@ -2,14 +2,14 @@ export class LeaveType {
     leaveTypeId: number;
     leaveTypeName: string;
     isActive: boolean|null;
-    eLWorkDayCal: boolean;
+    elWorkDayCal: boolean;
     remark: string
 
     constructor() {
         this.leaveTypeId = 0;
         this.leaveTypeName = "";
         this.isActive = null;
-        this.eLWorkDayCal = true;
+        this.elWorkDayCal = true;
         this.remark = "";
     }
 }

ab50399f3b56b638e065e28eb1b4b1fa00e681ce
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2024-11-06 10:25:06 +0600

    EmpNomineeInfo modified (Have Update Issue)

--- ClientApp/src/app/views/employee/service/emp-nominee-info.service.ts
+++ ClientApp/src/app/views/employee/service/emp-nominee-info.service.ts
@@ -20,11 +20,24 @@ export class EmpNomineeInfoService {
     return this.http.get<EmpNomineeInfoModel[]>(this.baseUrl + '/empNomineeInfo/get-EmpNomineeInfoByEmpId/' + id);
   }
 
-  saveEmpNomineeInfo(model: FormData) {
-    return this.http.post(this.baseUrl + '/empNomineeInfo/save-EmpNomineeInfo', model);
+  saveEmpNomineeInfo(model: any) {
+    const formData = this.toFormData(model);
+    return this.http.post(this.baseUrl + '/empNomineeInfo/save-EmpNomineeInfo', formData);
   }
   deleteEmpNomineeInfo(id: number) {
     return this.http.delete(this.baseUrl + '/empNomineeInfo/delete-EmpNomineeInfo/'+id);
+  }
+  
+  private toFormData(model: any): FormData {
+    const formData = new FormData();
+    for (const key of Object.keys(model)) {
+      if (model[key] instanceof File) {
+        formData.append(key, model[key], model[key].name);
+      } else {
+        formData.append(key, model[key]);
+      }
+    }
+    return formData;
   }
   
 }

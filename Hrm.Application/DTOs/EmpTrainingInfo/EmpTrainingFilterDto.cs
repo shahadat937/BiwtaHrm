d040e820df7c1b4199f48a8db682246b1601d935
Joy <joymahanta9@infinitytechltd.com>
2024-11-10 17:38:34 +0600

    Added Getemptraininginfo route with filtering

---
+++ Hrm.Application/DTOs/EmpTrainingInfo/EmpTrainingFilterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.DTOs.EmpTrainingInfo
+{
+    public class EmpTrainingFilterDto
+    {
+        public int? EmpId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? TrainingTypeId { get; set; }
+    }
+}

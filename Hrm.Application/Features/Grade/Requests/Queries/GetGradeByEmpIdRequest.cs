8c1eeb162d26a2aee6d69924a2f6717d3babcd23
Joy <joymahanta9@infinitytechltd.com>
2024-11-12 18:02:25 +0600

    Added feature to determine whether the user staff or officer, according to this, the acr form will be shown

---
+++ Hrm.Application/Features/Grade/Requests/Queries/GetGradeByEmpIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using Hrm.Application.DTOs.Grade;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Grade.Requests.Queries
+{
+    public class GetGradeByEmpIdRequest: IRequest<GradeDto>
+    {
+        public int EmpId { get; set; }
+    }
+}

af5a57a21aa3e56ab5a5b9e083171969c6f6c3a8
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-11-26 15:37:58 +0600

    modified ping route for device to allow only authorized device

---
+++ Hrm.Application/Features/AttendanceDevice/Handlers/Queries/GetDevicePingRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.Features.AttendanceDevice.Requests.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hrm.Application.Features.AttendanceDevice.Handlers.Queries
+{
+    public class GetDevicePingRequestHandler : IRequestHandler<GetDevicePingRequest,object>
+    {
+        private readonly IHrmRepository<Hrm.Domain.AttDevices> _repo;
+
+        public GetDevicePingRequestHandler(IHrmRepository<Domain.AttDevices> repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<object> Handle(GetDevicePingRequest request, CancellationToken cancellationToken)
+        {
+            var IsAuthorizedDevice = await _repo.Where(x => x.SN == request.SN && x.Status == true).AnyAsync();
+
+            if(!IsAuthorizedDevice)
+            {
+                return "Invalid Options";
+            }
+
+            return "OK";
+        }
+    }
+}
---
+++ Hrm.Application/Features/AttendanceDevice/Requests/Queries/GetDevicePingRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Hrm.Application.Features.AttendanceDevice.Requests.Queries
+{
+    public class GetDevicePingRequest: IRequest<object>
+    {
+        public string SN { get; set; }
+    }
+}

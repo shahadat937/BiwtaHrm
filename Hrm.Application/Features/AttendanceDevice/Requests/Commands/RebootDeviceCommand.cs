4ef03f6510727bdb1a258672b12bb69d62ee88d1
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-11-30 17:49:38 +0600

    Added route for rebooting attendance device for user

---
+++ Hrm.Application/Features/AttendanceDevice/Requests/Commands/RebootDeviceCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hrm.Application.Responses;
+using MediatR;
+
+namespace Hrm.Application.Features.AttendanceDevice.Requests.Commands
+{
+    public class RebootDeviceCommand : IRequest<BaseCommandResponse>
+    {
+        public int DeviceId { get; set; }
+    }
+}

90c38e454e7324f15d1fa67ec246dbbc161644ac
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2024-12-05 12:50:48 +0600

    unsubscribe added on emp id card generate component

--- ClientApp/src/app/views/employee/manage-employee/emp-id-card-generate/emp-id-card-generate.component.ts
+++ ClientApp/src/app/views/employee/manage-employee/emp-id-card-generate/emp-id-card-generate.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { BasicInfoModule } from '../../model/basic-info.module';
 import { EmpJobDetailsModule } from '../../model/emp-job-details.module';
 import { ActivatedRoute } from '@angular/router';
@@ -10,13 +10,14 @@ import { EmpPhotoSignService } from '../../service/emp-photo-sign.service';
 import { PersonalInfoModule } from '../../model/personal-info.module';
 import { EmpPersonalInfoService } from '../../service/emp-personal-info.service';
 import { cilPrint, cilArrowLeft } from '@coreui/icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-emp-id-card-generate',
   templateUrl: './emp-id-card-generate.component.html',
   styleUrl: './emp-id-card-generate.component.scss'
 })
-export class EmpIdCardGenerateComponent implements OnInit  {
+export class EmpIdCardGenerateComponent implements OnInit, OnDestroy  {
 
   empBasicInfo: BasicInfoModule = new BasicInfoModule;
   empJobDetails: EmpJobDetailsModule = new EmpJobDetailsModule;
@@ -28,6 +29,7 @@ export class EmpIdCardGenerateComponent implements OnInit  {
   
   empPhoto : string = '';
   empSignature : string = '';
+  subscription: Subscription[]=[]
 
   IdCardFront : string = `${this.empPhotoSignService.imageUrl}TempleteImage/Card_Front.jpg`;
   IdCardBack : string = `${this.empPhotoSignService.imageUrl}TempleteImage/Card_Back.jpg`;
@@ -49,11 +51,19 @@ export class EmpIdCardGenerateComponent implements OnInit  {
   ngOnInit(): void {
     this.handleRouteParams();
   }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.forEach(subs=>subs.unsubscribe());
+    }
+  }
 
   handleRouteParams() {
-    this.route.paramMap.subscribe((params) => {
+    this.subscription.push(
+      this.route.paramMap.subscribe((params) => {
       this.empId = Number(params.get('id'));
-    });
+    })
+    )
+    
 
     this.getEmpBasicInfoByEmpId();
     this.getEmpJobDetailsByEmpId();
@@ -63,23 +73,32 @@ export class EmpIdCardGenerateComponent implements OnInit  {
 
   
   getEmpBasicInfoByEmpId(){
-    this.manageEmployeeService.getEmpBasicInfoByEmpId(this.empId).subscribe((res) => {
+    this.subscription.push(
+      this.manageEmployeeService.getEmpBasicInfoByEmpId(this.empId).subscribe((res) => {
       this.empBasicInfo = res;
       this.pNo = res.personalFileNo;
-    });
+    })
+    )
+    
   }
   
   getEmpPersonalInfoByEmpId(){
-    this.empPersonalInfoService.findByEmpId(this.empId).subscribe((res) => {
+    this.subscription.push(
+      this.empPersonalInfoService.findByEmpId(this.empId).subscribe((res) => {
       this.empPersonalInfo = res;
       this.patchEmpPhoto(res.genderName);
-    });
+    })
+    )
+    
   }
   
   getEmpJobDetailsByEmpId(){
-    this.empJobDetailsService.findByEmpId(this.empId).subscribe((res) => {
+    this.subscription.push(
+this.empJobDetailsService.findByEmpId(this.empId).subscribe((res) => {
       this.empJobDetails = res;
-    });
+    })
+    )
+    
   }
   
   // getEmpPhotoSign(){
@@ -99,25 +118,28 @@ export class EmpIdCardGenerateComponent implements OnInit  {
   // }
 
   patchEmpPhoto(empGender: string){
-    this.empPhotoSignService.findByEmpId(this.empId).subscribe((res) => {
-      if(res){
-        this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/${res.photoUrl}`
-      }
-      else {
-        if(empGender){
-          const gender = empGender.charAt(0).toLowerCase();
-          if(gender == 'm'){
-            this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default_Male.jpg`
+    this.subscription.push(
+      this.empPhotoSignService.findByEmpId(this.empId).subscribe((res) => {
+        if(res){
+          this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/${res.photoUrl}`
+        }
+        else {
+          if(empGender){
+            const gender = empGender.charAt(0).toLowerCase();
+            if(gender == 'm'){
+              this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default_Male.jpg`
+            }
+            else {
+              this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default_Female.jpg`
+            }
           }
-          else {
-            this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default_Female.jpg`
+          else{
+            this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default.jpg`
           }
         }
-        else{
-          this.photoPreviewUrl = `${this.empPhotoSignService.imageUrl}/EmpPhoto/default.jpg`
-        }
-      }
-    })
+      })
+    )
+    
   }
 
   printIdCard() {

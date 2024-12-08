8b5a6f32f73f8e357cefaebdad520d1ef139db79
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-12-08 17:41:20 +0600

    Added route, handler for detailed summary of attendance for reporting

---
+++ Hrm.Application/Features/Attendance/Handlers/Queries/GetAttendanceSummaryDetailRequestHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.DTOs.Attendance;
+using Hrm.Application.Features.Attendance.Requests.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hrm.Application.Features.Attendance.Handlers.Queries
+{
+    public class GetAttendanceSummaryDetailRequestHandler : IRequestHandler<GetAttendanceSummaryDetailRequest,object>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAttendanceSummaryDetailRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<object> Handle(GetAttendanceSummaryDetailRequest request, CancellationToken cancellationToken)
+        {
+            var attendance = _unitOfWork.Repository<Hrm.Domain.Attendance>().Where(x => x.EmpId == request.EmpId &&
+                x.AttendanceDate >= DateOnly.FromDateTime(request.StartDate) &&
+                x.AttendanceDate <= DateOnly.FromDateTime(request.EndDate)
+            ).Include(x => x.AttendanceStatus).OrderBy(x=>x.AttendanceDate);
+            var attendanceDto = _mapper.Map<List<AttendanceDto>>(await attendance.ToListAsync());
+
+            List<AttendanceDto> attendanceHolidayDto = new List<AttendanceDto>();
+            for(DateTime cur = request.StartDate; cur<=request.EndDate; cur = cur.AddDays(1))
+            {
+
+            }
+
+            return "";
+        }
+    }
+}

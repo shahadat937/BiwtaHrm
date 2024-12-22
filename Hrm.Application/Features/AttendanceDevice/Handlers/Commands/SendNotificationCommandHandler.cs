2cdefd881f81608609de20037737eb87ed4cb49d
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-12-22 18:12:19 +0600

    Added signalR hub for realtime communication, added test route to trigger event

---
+++ Hrm.Application/Features/AttendanceDevice/Handlers/Commands/SendNotificationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hrm.Application.Features.AttendanceDevice.Requests.Commands;
+using Hrm.Infrastructure.SignalRHub;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Hrm.Application.Features.AttendanceDevice.Handlers.Commands
+{
+    public class SendNotificationCommandHandler : IRequestHandler<SendNotificationCommand,object>
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public SendNotificationCommandHandler(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task<object> Handle(SendNotificationCommand request, CancellationToken cancellationToken)
+        {
+            await _hubContext.Clients.All.SendAsync("newDevice", "New Device Available");
+            await _hubContext.Clients.All.SendAsync("notification", "New Message Available");
+
+            return "Ok";
+        }
+    }
+}
---
+++ Hrm.Application/Features/AttendanceDevice/Requests/Commands/SendNotificationCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Hrm.Application.Features.AttendanceDevice.Requests.Commands
+{
+    public class SendNotificationCommand : IRequest<object>
+    {
+
+    }
+}

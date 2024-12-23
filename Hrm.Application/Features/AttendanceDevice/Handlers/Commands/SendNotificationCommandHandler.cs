5a0cd746079744832bbd4ebdeaf4780c68fb490c
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-12-23 17:56:09 +0600

    Added signalR in Device Table Data Command handler

--- Hrm.Application/Features/AttendanceDevice/Handlers/Commands/SendNotificationCommandHandler.cs
+++ Hrm.Application/Features/AttendanceDevice/Handlers/Commands/SendNotificationCommandHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hrm.Application.Contracts.Persistence;
 using Hrm.Application.Features.AttendanceDevice.Requests.Commands;
 using Hrm.Infrastructure.SignalRHub;
 using MediatR;
@@ -13,15 +14,19 @@ namespace Hrm.Application.Features.AttendanceDevice.Handlers.Commands
     public class SendNotificationCommandHandler : IRequestHandler<SendNotificationCommand,object>
     {
         private readonly IHubContext<NotificationHub> _hubContext;
+        private readonly IHrmRepository<Hrm.Domain.Attendance> _attendanceRepo;
 
-        public SendNotificationCommandHandler(IHubContext<NotificationHub> hubContext)
+        public SendNotificationCommandHandler(IHubContext<NotificationHub> hubContext, IHrmRepository<Domain.Attendance> attendanceRepo)
         {
             _hubContext = hubContext;
+            _attendanceRepo = attendanceRepo;
         }
 
         public async Task<object> Handle(SendNotificationCommand request, CancellationToken cancellationToken)
         {
-            await _hubContext.Clients.All.SendAsync("newDevice", "New Device Available");
+
+            var attendance = await _attendanceRepo.Get(16);
+            await _hubContext.Clients.All.SendAsync("newDevice", attendance);
             await _hubContext.Clients.All.SendAsync("notification", "New Message Available");
 
             return "Ok";

296ba692357ba567d2debdfc718e7fd836cd77ce
Joy Mahanta <joymahanta9@infinitytechltd.com>
2025-01-19 16:55:59 +0600

    Added FormSection dtos

---
+++ Hrm.Application/DTOs/FormSection/Validators/CreateFormSectionDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Hrm.Application.DTOs.FormSection.Validators
+{
+    public class CreateFormSectionDtoValidator: AbstractValidator<CreateFormSectionDto>
+    {
+        public CreateFormSectionDtoValidator()
+        {
+            Include(new IFormSectionDtoValidator());
+            RuleFor(x => x.IsActive).NotNull().WithMessage("{PropertyName} is required");
+        }
+    }
+}

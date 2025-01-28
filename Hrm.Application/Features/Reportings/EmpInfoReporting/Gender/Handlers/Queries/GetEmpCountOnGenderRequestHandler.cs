62fd3dc83312fddadd2688cc9515e87c6b5c64ef
infinityaziz <abdulaziz@infinitytechltd.com>
2025-01-28 14:57:18 +0600

    create maritalStatus and Gender reporting API

---
+++ Hrm.Application/Features/Reportings/EmpInfoReporting/Gender/Handlers/Queries/GetEmpCountOnGenderRequestHandler.cs
@@ -0,0 +1,58 @@
+﻿using Hrm.Application.Contracts.Persistence;
+using Hrm.Application.DTOs.Reporting;
+using Hrm.Application.Features.Reportings.EmpInfoReporting.Gender.Requests.Queries;
+using Hrm.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Reportings.EmpInfoReporting.Gender.Handlers.Queries
+{
+    public class GetEmpCountOnGenderRequestHandler : IRequestHandler<GetEmpCountOnGenderRequest, EmpCountOnReportingDto>
+    {
+        private readonly IHrmRepository<Domain.Gender> _GenderRepository;
+        private readonly IHrmRepository<EmpBasicInfo> _EmpBasicInfoRepository;
+
+        public GetEmpCountOnGenderRequestHandler(
+            IHrmRepository<Domain.Gender> GenderRepository, IHrmRepository<EmpBasicInfo> EmpBasicInfoRepository)
+        {
+            _GenderRepository = GenderRepository;
+            _EmpBasicInfoRepository = EmpBasicInfoRepository;
+        }
+
+        public async Task<EmpCountOnReportingDto> Handle(GetEmpCountOnGenderRequest request, CancellationToken cancellationToken)
+        {
+            var Genders = await _GenderRepository.GetAll();
+            var result = new EmpCountOnReportingDto();
+
+            result.TotalAssigned = await _EmpBasicInfoRepository.CountAsync(x => x.EmpPersonalInfo.FirstOrDefault().GenderId != null &&
+                (request.DepartmentId == 0 || x.EmpJobDetail.FirstOrDefault().DepartmentId == request.DepartmentId) &&
+                (request.SectionId == 0 || x.EmpJobDetail.FirstOrDefault().SectionId == request.SectionId));
+            result.TotalNull = await _EmpBasicInfoRepository.CountAsync(x => x.EmpPersonalInfo.FirstOrDefault().GenderId == null &&
+                (request.DepartmentId == 0 || x.EmpJobDetail.FirstOrDefault().DepartmentId == request.DepartmentId) &&
+                (request.SectionId == 0 || x.EmpJobDetail.FirstOrDefault().SectionId == request.SectionId));
+
+            var GenderInfoList = new List<CountReportingInfo>();
+
+            foreach (var Gender in Genders)
+            {
+                var GenderInfo = new CountReportingInfo
+                {
+                    Id = Gender.GenderId,
+                    Name = Gender.GenderName ?? "",
+                    Count = await _EmpBasicInfoRepository.CountAsync(x => x.EmpPersonalInfo.FirstOrDefault().GenderId == Gender.GenderId &&
+                        (request.DepartmentId == 0 || x.EmpJobDetail.FirstOrDefault().DepartmentId == request.DepartmentId) &&
+                        (request.SectionId == 0 || x.EmpJobDetail.FirstOrDefault().SectionId == request.SectionId))
+                };
+                GenderInfoList.Add(GenderInfo);
+            }
+
+            result.CountReportingInfo = GenderInfoList;
+
+            return result;
+        }
+    }
+}
---
+++ Hrm.Application/Features/Reportings/EmpInfoReporting/Gender/Requests/Queries/GetEmpCountOnGenderRequest.cs
@@ -0,0 +1,17 @@
+﻿using Hrm.Application.DTOs.Reporting;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.Features.Reportings.EmpInfoReporting.Gender.Requests.Queries
+{
+    public class GetEmpCountOnGenderRequest : IRequest<EmpCountOnReportingDto>
+    {
+        public int? DepartmentId { get; set; }
+        public int? SectionId { get; set; }
+    }
+}
+

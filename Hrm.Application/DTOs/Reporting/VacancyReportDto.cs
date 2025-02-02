be3b3986398364903b5aa0e7ba1524d868af996d
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2025-02-02 12:35:58 +0600

    Language modified in API

---
+++ Hrm.Application/DTOs/Reporting/VacancyReportDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Application.DTOs.Reporting
+{
+    public class VacancyReportDto
+    {
+        public int? TotalPost { get; set; }
+        public int? TotalInService { get; set; }
+        public int? TotalVacant { get; set; }
+        public List<VacancyDetailsDto> VacancyDetailsDto { get; set; }
+    }
+    public class VacancyDetailsDto
+    {
+        public string? DepartmentName { get; set; }
+        public string? SectionName { get; set; }
+        public string? DesignationName { get; set; }
+        public int? TotalPost { get; set; }
+        public int? TotalInService { get; set; }
+        public int? TotalVacantPost { get; set; }
+    }
+}

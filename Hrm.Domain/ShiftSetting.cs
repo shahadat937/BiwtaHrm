8dcdebcf3348b1c612d52a6dffbe84d3521fa88f
MdKhairuzzamanRakib <mdkhairuzzamanrakib153@gmail.com>
2025-04-24 17:08:43 +0600

    Working with Shift

---
+++ Hrm.Domain/ShiftSetting.cs
@@ -0,0 +1,24 @@
+﻿using Hrm.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrm.Domain
+{
+    public class ShiftSetting : BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public string? SettingName { get; set; }
+        public int? ShiftTypeId { get; set; }
+        public TimeOnly? StartTime { get; set; }
+        public TimeOnly? EndTime { get; set; }
+        public TimeOnly? BufferTime { get; set; }
+        public TimeOnly? AbsentTime { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public bool? IsActive { get; set; }
+        public string? Remark { get; set; }
+    }
+}
